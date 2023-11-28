fc804c7e22a020c2f28084c75de17ea59c54ddd2
Ethan2541 <eluong41@gmail.com>
2023-11-28 23:30:09 +0100

    Code restructured

---
+++ ShortestPaths.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+class ShortestPaths:
+    def __init__(self, graph, source):
+        self.graph = graph
+        self.source = source
+        self.n_iterations = 0
+        self.distances = {}
+        self.predecessors = {}
+        self.init_distances_and_predecessors()
+
+    def init_distances_and_predecessors(self):
+        for v in self.graph.nodes:
+            if v == self.source:
+                self.distances[v] = 0
+            else:
+                self.distances[v] = np.inf
+            self.predecessors[v] = None
+
+    def update_all_vertices(self):
+        vertices_to_update = [v for v in self.graph.nodes if v != self.source]        
+        for v in vertices_to_update:
+            self.update(v)
+            self.n_iterations += 1
+
+    def update(self, v):
+        incoming_edges_of_vertex = self.graph.in_edges(v)
+        for predecessor, _ in incoming_edges_of_vertex:
+            dpredecessor = self.distances[predecessor]
+            dv = self.distances[v]
+            weight = self.graph[predecessor][v]['weight']
+            if dpredecessor + weight < dv:   
+                self.distances[v] = dpredecessor + weight
+                self.predecessors[v] = predecessor
---
+++ algorithms.py
@@ -0,0 +1,66 @@
+from ShortestPaths import ShortestPaths
+import networkx as nx
+import numpy as np
+
+
+def compute_shortest_paths(graph: nx.DiGraph, source:int):
+    shortest_paths = ShortestPaths(graph, source)
+    shortest_paths.update_all_vertices()
+    distances = shortest_paths.distances
+    predecessors = shortest_paths.predecessors
+    n_iterations = shortest_paths.n_iterations
+    return distances, predecessors, n_iterations
+
+
+# The GloutonFas algorithm aims to determine an order of the vertices for the Bellman-Ford algorithm
+# First, we choose all the sources (vertices with no predecessors=)
+# Then, we determine the targets (vertices with no successor)
+# If there is neither any source nor target, the next greatest vertex is the one whose the difference between the number of successors and predecessors is maximal
+def glouton_fas(graph: nx.DiGraph):
+    s1 = []
+    s2 = []
+    graph_copy = graph.copy()
+
+    while len(graph_copy.nodes) > 0:
+        graph_copy, removed_sources = remove_sources(graph_copy)
+        s1 += removed_sources
+
+        graph_copy, removed_targets = remove_targets(graph_copy)
+        s2 += removed_targets
+
+        if len(graph_copy.nodes) > 0:
+            v = find_vertex_with_maximal_gap_out_in_degrees(graph_copy)
+            s1.append(v)
+            graph_copy.remove_node(v)
+
+    return s1+s2
+
+
+def remove_sources(graph: nx.DiGraph):
+    sources = [x for x in graph.nodes() if graph.in_degree(x)==0]
+    removed_sources = []
+    while len(sources) > 0:
+        removed_sources.append(sources[0])
+        graph.remove_node(sources[0])
+        sources = [x for x in graph.nodes() if graph.in_degree(x)==0]
+    return graph, removed_sources
+
+
+def remove_targets(graph: nx.DiGraph):
+    targets = [x for x in graph.nodes() if graph.out_degree(x)==0]
+    removed_targets = []
+    while targets: 
+        removed_targets.append(targets[0])
+        graph.remove_node(targets[0])
+        targets = [x for x in graph.nodes() if graph.out_degree(x)==0]
+    return graph, removed_targets
+
+
+def find_vertex_with_maximal_gap_out_in_degrees(graph: nx.DiGraph):
+    max_gap = -np.inf
+    vertex_max = None
+    for v in graph.nodes:
+        if max_gap < graph.out_degree(v) - graph.in_degree(v):
+            max_gap = graph.out_degree(v) - graph.in_degree(v)
+            vertex_max = v
+    return vertex_max
